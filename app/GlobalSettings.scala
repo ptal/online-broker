09839e9be7dd7892072276720d9c84d93d15fd0f
Pierre Talbot <ptalbot@hyc.io>
2013-12-18 09:03:53 +0100

    Add the currency status table and delete the date from ExchangeRates.

--- app/GlobalSettings.scala
+++ app/GlobalSettings.scala
@@ -1,6 +1,5 @@
 
 
-import org.joda.time.DateTime
 import play.api._
 
 import scala.slick.session.Database
@@ -9,7 +8,7 @@ import scala.slick.driver.H2Driver.simple._
 // Use the implicit threadLocalSession
 import Database.threadLocalSession
 
-import daos.{ExchangeRates, Transfer, UserTable, DBAccess}
+import daos.{ExchangeRates, Transfer, UserTable, CurrencyStatus, DBAccess}
 
 object Global extends GlobalSettings {
 
@@ -18,13 +17,17 @@ object Global extends GlobalSettings {
     val INITIAL_MONEY = 300000
 
     // Fills the database (for testing purposes)
-    /*DBAccess.db withSession {
-       (UserTable.ddl ++ Transfer.ddl ++ ExchangeRates.ddl).create
+
+    DBAccess.db withSession {
+       (UserTable.ddl ++ Transfer.ddl ++ ExchangeRates.ddl ++ CurrencyStatus.ddl).create
 
        Query(ExchangeRates).delete
-       ExchangeRates.add("Dollar", 1.0, new java.sql.Date(DateTime.now().getMillis))
-       ExchangeRates.add("Euro", 1.374, new java.sql.Date(DateTime.now().getMillis))
-       ExchangeRates.add("Pound", 1.626, new java.sql.Date(DateTime.now().getMillis))
+       ExchangeRates.add("DOL", "Dollar", 1.0)
+       ExchangeRates.add("EUR", "Euro", 1.374)
+       ExchangeRates.add("POU", "Pound", 1.626)
+
+       Query(CurrencyStatus).delete
+       CurrencyStatus.init
 
        val users = List("Pierre Talbot", "Inigo Mediavilla")
 
@@ -34,7 +37,7 @@ object Global extends GlobalSettings {
        // Insert some suppliers
        Query(Transfer).delete
        idsUsers.foreach(Transfer.add("Dollar", INITIAL_MONEY, _))
-    }*/
+    }
   }
 
 }
--- app/daos/CurrencyDAO.scala
+++ app/daos/CurrencyDAO.scala
@@ -2,41 +2,54 @@ package daos
 
 import models.Currency
 
+import org.joda.time.DateTime
 import scala.slick.session.Database
 import scala.slick.driver.H2Driver.simple._
 import java.sql.Date
 
-object ExchangeRates extends Table[(Long, String, Double, Date)]("ExchangeRates") {
+object ExchangeRates extends Table[(Long, String, String, Double)]("ExchangeRates") {
 
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def currencyAcronym = column[String]("currencyAcronym")
   def currencyName = column[String]("currencyName")
   def exchangeRate = column[Double]("exchangeRate")
-  def date = column[Date]("date")
 
 
-  def * = id ~ currencyName ~ exchangeRate ~ date
+  def * = id ~ currencyAcronym ~ currencyName ~ exchangeRate
 
-  def autoInc = currencyName ~ exchangeRate ~ date returning id
+  def autoInc = currencyAcronym ~ currencyName ~ exchangeRate returning id
 
-  /**
-   * Inserts a new user in the DB with its id automatically generated.
-   *
-   * @param userName name of the user
-   * @return the id of the new created user
-   */
-  def add(currencyName: String, exchangeRate: Double, date: Date)(implicit s: Session) : Long =
-    autoInc.insert(currencyName, exchangeRate, date)
+  def add(currencyAcronym: String, currencyName: String, exchangeRate: Double)(implicit s: Session) : Long =
+    autoInc.insert(currencyAcronym, currencyName, exchangeRate)
 
+}
+
+object CurrencyStatus extends Table[(Long, Date)]("CurrencyStatus") {
+  
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+  def lastExchangeRateUpdate = column[Date]("lastExchangeRateUpdate")
+
+  def * = id ~ lastExchangeRateUpdate
+  def autoInc = lastExchangeRateUpdate returning id
+
+  def init(implicit s: Session): Long =
+    autoInc.insert(new java.sql.Date(DateTime.now().getMillis))
 }
 
 object CurrencyDAO {
 
   def findCurrentExchangeRate(name: String): Option[Currency] = {
     DBAccess.db withSession { implicit session =>
-      Query(ExchangeRates).filter(_.currencyName === name).sortBy(_.date).firstOption.map {
-        case (_, name, rate, _) => Currency(name, rate)
+      Query(ExchangeRates).filter(_.currencyName === name).sortBy(_.currencyName).firstOption.map {
+        case (_, _, name, rate) => Currency(name, rate)
       }
     }
   }
+
+  def updateLastRateDate(lastExchangeRateUpdate: Date) =
+    DBAccess.db withSession { implicit session : Session =>
+      val q = for { a <- CurrencyStatus } yield a.lastExchangeRateUpdate
+      q.update(lastExchangeRateUpdate)
+   }
 
 }
