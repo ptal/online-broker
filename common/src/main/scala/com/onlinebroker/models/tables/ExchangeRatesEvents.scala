305633c6f82b1411b13db2fb324dde28e95986db
Pierre Talbot <ptalbot@hyc.io>
2014-01-11 03:34:39 +0100

    Add methods findExchangeRatesEventById and findLastRateByCurrencyAcronym (important to find the updated rates).

--- common/src/main/scala/com/onlinebroker/models/tables/ExchangeRatesEvents.scala
+++ common/src/main/scala/com/onlinebroker/models/tables/ExchangeRatesEvents.scala
@@ -3,9 +3,11 @@ package com.onlinebroker.models.tables
 import scala.slick.session.Database
 import scala.slick.driver.MySQLDriver.simple._
 
-import com.onlinebroker.models.ExchangeRatesEvent
+import com.onlinebroker.models._
 
 object ExchangeRatesEvents extends Table[ExchangeRatesEvent]("ExchangeRatesEvents") {
+
+  val eventName = "ExchangeRatesEvent"
 
   def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
   def base = column[Long]("Currency")
@@ -16,4 +18,32 @@ object ExchangeRatesEvents extends Table[ExchangeRatesEvent]("ExchangeRatesEvent
 
   def insert(rates: ExchangeRatesEvent)(implicit s: Session): Long = 
     autoInc.insert(rates.base)
+
+  def findExchangeRatesEventById(id: Long)
+    (implicit s: Session): \/[OnlineBrokerError, ExchangeRatesEvents] = 
+  {
+    for(lastEvent <- GameEvents.findLastEventByName(eventName))
+    yield {
+      Query(ExchangeRatesEvents)
+      .filter(_.id === lastEvent.event)
+      .firstOption match {
+        None => -\/(InternalServerError("ExchangeRatesEvents.findExchangeRatesEventById: Event ID retrieved in GameEvents table does not match any entry in this table."))
+        Some(rates) => \/-(rates)
+      }
+    }
+  }
+
+  def findLastRateByCurrencyAcronym(currencyAcronym: String)
+    (implicit s: Session): \/[OnlineBrokerError, ExchangeRate] =
+  {
+    // The second query should never fails. (acts as a debug assertion).
+    for(event <- GameEvents.findLastEventByName(eventName);
+        exchangeRatesEvent <- findExchangeRatesEventById(event.event);
+        currency <- Currency.findByAcronym(currencyAcronym);
+        exchangeRate <- ExchangeRates.findExchangeRate(exchangeRatesEvent, currency)) 
+    yield {
+      exchangeRate
+    }
+  }
+
 }
