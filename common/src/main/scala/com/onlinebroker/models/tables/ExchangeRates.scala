2ee87c510d22bc8d834edd137d87ea55a38b4747
Pierre Talbot <ptalbot@hyc.io>
2014-01-11 03:35:28 +0100

    Add method to find an exchange rate with the associated event and the currency.

--- common/src/main/scala/com/onlinebroker/models/tables/ExchangeRates.scala
+++ common/src/main/scala/com/onlinebroker/models/tables/ExchangeRates.scala
@@ -16,8 +16,34 @@ object ExchangeRates extends Table[ExchangeRate]("ExchangeRates") {
 
   def currencyFK = foreignKey("TR_CURRENCY_FK", currency, Currencies)(_.id)
   def eventFK = foreignKey("TR_EVENT_FK", event, ExchangeRatesEvents)(_.id)
+  def uniqueEventCurrency = index("UNIQUE_EVENT_CURRENCY", (currency, event), unique = true)
   def autoInc = rate ~ currency ~ event returning id
 
   def insert(rate: ExchangeRate)(implicit s: Session): Long = 
     autoInc.insert(rate.rate, rate.currency, rate.event)
+
+  private def queryByEvent(exchangeRatesEvent: ExchangeRatesEvent)
+    (implicit s: Session) =
+  {
+    Query(ExchangeRates)
+    .filter(_.event === exchangeRatesEvent.id)
+  }
+
+  def findExchangeRatesByEvent(exchangeRatesEvent: ExchangeRatesEvent)
+    (implicit s: Session): List[ExchangeRate] =
+  {
+    queryByEvent(exchangeRatesEvent).list
+  }
+
+  def findExchangeRate(exchangeRatesEvent: ExchangeRatesEvent, currency: Long)
+    (implicit s: Session): \/[OnlineBrokerError, ExchangeRate] =
+  {
+    queryByEvent(exchangeRatesEvent)
+    .filter(_.currency = currency)
+    .firstOption match {
+      case None => -\/(InternalServerError("ExchangeRates.findExchangeRate: No exchange rate for this event and currency found."))
+      case Some(exchangeRate) => \/-(exchangeRate)
+    }
+  }
+
 }
