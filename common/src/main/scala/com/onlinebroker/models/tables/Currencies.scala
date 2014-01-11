e6776c4e7d6c4ec2f290d2e6f399a61dcbdd6ec9
Pierre Talbot <ptalbot@hyc.io>
2014-01-11 03:35:56 +0100

    Add a method to find a currency by acronym.

--- common/src/main/scala/com/onlinebroker/models/tables/Currencies.scala
+++ common/src/main/scala/com/onlinebroker/models/tables/Currencies.scala
@@ -13,8 +13,20 @@ object Currencies extends Table[Currency]("Currencies") {
 
   def * = id.? ~ acronym ~ fullName <> (Currency, Currency.unapply _)
 
+  def uniqueAcronym = index("UNIQUE_ACRONYM", acronym, unique = true)
   def autoInc = acronym ~ fullName returning id
 
   def insert(currency: Currency)(implicit s: Session) : Long = 
     autoInc.insert(currency.acronym, currency.fullName)
+
+  def findByAcronym(currencyAcronym: String)
+    (implicit s: Session): \/[OnlineBrokerError, Currency] =
+  {
+    Query(Currencies)
+    .filter(_.acronym === currencyAcronym)
+    .firstOption match {
+      case None => -\/(BadCurrencyAcronym(currencyAcronym))
+      case Some(currency) => \/-(currency)
+    }
+  }
 }
